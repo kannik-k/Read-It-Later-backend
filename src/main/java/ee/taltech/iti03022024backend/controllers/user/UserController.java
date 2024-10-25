821070063e22d8580ae7971b5e6b39a07180ed52
aekald <aekald@taltech.ee>
2024-10-25 14:28:38 +0300

    api

--- src/main/java/ee/taltech/iti03022024backend/controllers/user/UserController.java
+++ src/main/java/ee/taltech/iti03022024backend/controllers/user/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RequiredArgsConstructor
-@RequestMapping()
+@RequestMapping("api")
 @RestController
 public class UserController {
     private final UserService userService;
