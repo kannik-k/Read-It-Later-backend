0e1735792db129deb91f0dac6efa5347883dc46a
Karoliina <kannik@taltech.ee>
2024-11-04 18:27:45 +0200

    genre controller, dto, entity, mapper, repository, service

---
+++ src/main/java/ee/taltech/iti03022024backend/services/genre/GenreService.java
@@ -0,0 +1,36 @@
+package ee.taltech.iti03022024backend.services.genre;
+
+import ee.taltech.iti03022024backend.dto.genre.GenreDto;
+import ee.taltech.iti03022024backend.entities.genre.GenreEntity;
+import ee.taltech.iti03022024backend.exceptions.NotFoundException;
+import ee.taltech.iti03022024backend.mappers.genre.GenreMapper;
+import ee.taltech.iti03022024backend.repositories.genre.GenreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class GenreService {
+    private final GenreMapper genreMapper;
+    private final GenreRepository genreRepository;
+
+    public List<GenreDto> getAllGenres() {
+        return genreMapper.toDtoList(genreRepository.findAll());
+    }
+
+    public GenreDto getGenreById(long id) throws NotFoundException {
+        GenreEntity genre = genreRepository.findById(id).orElseThrow(() -> new NotFoundException("Genre does not exist"));
+        return genreMapper.toDto(genre);
+    }
+
+    public GenreDto getGenreByName(String name) throws NotFoundException {
+        List<GenreEntity> genres = genreRepository.findAll();
+        GenreEntity genre = genres.stream()
+                .filter(genreName-> genreName.getGenre().equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException("Genre does not exist"));
+        return genreMapper.toDto(genre);
+    }
+}
