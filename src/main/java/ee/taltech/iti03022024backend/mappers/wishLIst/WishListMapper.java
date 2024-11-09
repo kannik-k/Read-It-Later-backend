8d8673f92170717caa2b6e748d080e23880f6ad5
Karoliina <kannik@taltech.ee>
2024-11-09 20:32:10 +0200

    wish list liquibases; post, delete ja getByUserId endpoindid

---
+++ src/main/java/ee/taltech/iti03022024backend/mappers/wishLIst/WishListMapper.java
@@ -0,0 +1,17 @@
+package ee.taltech.iti03022024backend.mappers.wishLIst;
+
+import ee.taltech.iti03022024backend.dto.wishList.WishListDtoIn;
+import ee.taltech.iti03022024backend.dto.wishList.WishListDtoOut;
+import ee.taltech.iti03022024backend.entities.wishList.WishListEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface WishListMapper {
+
+    WishListDtoOut toDto(WishListEntity wishListEntity);
+    WishListEntity toEntity(WishListDtoIn wishListDtoIn);
+    List<WishListDtoOut> toDtoList(List<WishListEntity> wishListEntityList);
+}
