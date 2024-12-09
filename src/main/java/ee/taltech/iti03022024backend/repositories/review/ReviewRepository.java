1c4e035c7ad7eff33757f04d6133cc43bbc030d5
Karoliina <kannik@taltech.ee>
2024-12-10 01:53:12 +0200

    review tests (not going great)

--- src/main/java/ee/taltech/iti03022024backend/repositories/review/ReviewRepository.java
+++ src/main/java/ee/taltech/iti03022024backend/repositories/review/ReviewRepository.java
@@ -4,8 +4,6 @@ import ee.taltech.iti03022024backend.entities.review.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long>, JpaSpecificationExecutor<ReviewEntity> {
-    Optional<ReviewEntity> findByBookId(Long bookId);
 }
