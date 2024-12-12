d9764f48f293e26e40ecb2a300f527420bee7756
Faina <fadoms@taltech.ee>
2024-12-12 13:44:59 +0200

    bugfix

--- src/test/java/ee/taltech/iti03022024backend/controllers/review/ReviewControllerTest.java
+++ src/test/java/ee/taltech/iti03022024backend/controllers/review/ReviewControllerTest.java
@@ -1,5 +1,6 @@
 package ee.taltech.iti03022024backend.controllers.review;
 
+import ee.taltech.iti03022024backend.AbstractIntegrationTest;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-class ReviewControllerTest {
+class ReviewControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mvc;
@@ -29,6 +31,7 @@ class ReviewControllerTest {
         """;
 
         mvc.perform(post("/api/review")
+                        .with(user("1").password("password1").roles("USER"))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(reviewDtoIn))
                 .andExpect(status().isOk())
@@ -43,8 +46,8 @@ class ReviewControllerTest {
                         .param("page", "0")
                         .param("size", "10"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").isArray())
-                .andExpect(jsonPath("$.content[0].review").value("Good"))
-                .andExpect(jsonPath("$.content[1].review").value("bad"));
+                .andExpect(jsonPath("$.reviews").isArray())
+                .andExpect(jsonPath("$.reviews[0].review").value("Good"))
+                .andExpect(jsonPath("$.reviews[1].review").value("bad"));
     }
 }
